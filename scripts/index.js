b0759d09c21050b9d942c0165d2f0392f2f24e5f
Strelkova Olga <strelod@yandex.ru>
2021-09-05 09:57:49 +0300

    feat: submit on JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,18 @@
 let editProfileButton = document.querySelector(".profile__edit-button"); //кнопка редактирования профайла
 let popup = document.querySelector(".popup"); //всплывающее окно с затемнением фона
 let closePopupButton = document.querySelector(".popup__close"); //закрытие попапа
+let form = document.querySelector(".popup__form"); //форма заполнения профиля
 
-let NameInputPopup = document.querySelector(".popup__field_type_name"); //имя в форме
-let JobInputPopup = document.querySelector(".popup__field_type_job"); //род деятельности в форме
-let ProfileName = document.querySelector(".profile__name"); //имя в шапке
-let ProfileJob = document.querySelector(".profile__description"); //род деятельности в шапке
+let nameInputPopup = document.querySelector(".popup__field_type_name"); //имя в форме
+let jobInputPopup = document.querySelector(".popup__field_type_job"); //род деятельности в форме
+let profileName = document.querySelector(".profile__name"); //имя в шапке
+let profileJob = document.querySelector(".profile__description"); //род деятельности в шапке
 
 //функция переключения попапа с подгрузкой имени и рода деятельности из шапки
 function togglePopup() {
   if (!popup.classList.contains("popup_is-opened")) {
-    NameInputPopup.value = ProfileName.textContent;
-    JobInputPopup.value = ProfileJob.textContent;
+    nameInputPopup.value = profileName.textContent;
+    jobInputPopup.value = profileJob.textContent;
   }
   popup.classList.toggle("popup_is-opened");
 }
@@ -21,3 +22,11 @@ editProfileButton.addEventListener("click", togglePopup);
 
 //событие переключения состояния при нажатии кнопки закрыть форму
 closePopupButton.addEventListener("click", togglePopup);
+
+//заполнение профиля из инпутов при сабмите формы
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  togglePopup();
+  profileName.textContent = nameInputPopup.value;
+  profileJob.textContent = jobInputPopup.value;
+});
