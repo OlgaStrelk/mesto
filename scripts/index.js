814fa32961be0b86b3bf5b5e777f4c5ccc962a8e
Strelkova Olga <strelod@yandex.ru>
2021-09-08 21:55:44 +0300

    JS in progress 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,12 @@
 const editProfileButton = document.querySelector(".profile__edit-button"); //кнопка редактирования профайла
-const popup = document.querySelector(".popup"); //всплывающее окно с затемнением фона
-const popupProfile = document.querySelector(".popup_type_profile-info"); //попап профиля
-const closePopupButton = document.querySelector(".popup__close"); //закрытие попапа
+const addPostButton = document.querySelector('.profile__add-button'); //кнопка добавления своего места
+
+const popupProfile = document.querySelector(".popup_type_profile"); //попап профиля
+const popupPost = document.querySelector('.popup_type_add-card');
+
+const closeProfileButton = document.querySelector(".popup__close_type_profile"); //закрытие попапа
+const closePostButton = document.querySelector('.popup__close_type_add-card');
+
 const form = document.querySelector(".popup__form"); //форма заполнения профиля
 
 const nameInputPopup = document.querySelector(".popup__field_type_name"); //имя в форме
@@ -9,24 +14,23 @@ const jobInputPopup = document.querySelector(".popup__field_type_job"); //род
 const profileName = document.querySelector(".profile__title"); //имя в шапке
 const profileJob = document.querySelector(".profile__description"); //род деятельности в шапке
 
-const addPostButton = document.querySelector('.profile__add-button'); //кнопка добавления своего места
-
-//функция переключения попапа с подгрузкой имени и рода деятельности из шапки и замена имени и рода деятельности после закрытия
-function togglePopup() {
-  if (!popupProfile.classList.contains("popup_is-opened")) {
-    nameInputPopup.value = profileName.textContent;
-    jobInputPopup.value = profileJob.textContent;
-  }
-  popup.classList.toggle("popup_is-opened");
+//функция переключения попапа
+function togglePopup(popupName) {
+  popupName.classList.toggle("popup_is-opened");
 }
 
-//событие переключения состояния при нажатии кнопки редактировать форму
-editProfileButton.addEventListener("click", togglePopup);
+//открытие с подгрузкой имени и рода деятельности из шапки и замена имени и рода деятельности после закрытия
 
-addPostButton.addEventListener("click", togglePopup);
+
+//событие переключения состояния при нажатии кнопки редактировать форму
+editProfileButton.addEventListener("click", togglePopup(popupProfile));
+
+addPostButton.addEventListener("click", togglePopup(popupPost));
 
 //событие переключения состояния при нажатии кнопки закрыть форму
-closePopupButton.addEventListener("click", togglePopup);
+closeProfileButton.addEventListener("click", togglePopup(popupProfile));
+
+
 
 //отправка формы без действия по умолчанию
 function formSubmit(event) {
