ff1d099c2ac662de9a6ec432b41a0a4de744532c
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-04-15 15:56:02 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { Section } from "../components/Section.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
 import { UserInfo } from "../components/UserInfo.js";
+import {  } from "../utils/consts.js"
 
 const formProfileValidator = new FormValidator(validationConfig, formProfile);
 const formCardValidator = new FormValidator(validationConfig, formCard);
