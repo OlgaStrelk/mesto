642a2a47c4de462d257bc64d614656508f2fd370
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-04-15 15:46:18 +0300

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,9 +2,9 @@
 //items — это массив данных, которые нужно добавить на страницу при инициализации класса.
 //Свойство renderer — это функция, которая отвечает за создание и отрисовку данных на странице.
 export class Section {
-  constructor({ items, renderer }, containerSelector) {
+  constructor({ items, renderer }, container) {
     this._initialArray = items;
-    this._container = containerSelector;
+    this._container = container;
     this._renderer = renderer;
   }
 
