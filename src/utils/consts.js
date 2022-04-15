4f290d06d825ddae70133215643933b01e050868
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-04-15 15:52:55 +0300

    Update consts.js

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,1 +1,16 @@
+export const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__field",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "popup__submit_disabled",
+  inputErrorClass: "popup__field_type_error",
+  errorClass: "popup__error_visible",
+};
 
+export const buttonEditProfile = document.querySelector(".profile__edit-button");
+export const buttonAddCard = document.querySelector(".profile__add-button");
+export const formProfile = document.querySelector(".popup__form_type_profile");
+export const formCard = document.querySelector(".popup__form_type_add-card");
+export const nameInput = document.querySelector(".popup__field_type_name");
+export const jobInput = document.querySelector(".popup__field_type_job");
+export const cardsContainer = document.querySelector(".cards");
