d01b3020fe560ea86db84472366c2cf281895266
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-04-15 15:54:52 +0300

    Update consts.js

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -7,6 +7,34 @@ export const validationConfig = {
   errorClass: "popup__error_visible",
 };
 
+
+export const initialCards = [
+  {
+    name: "Рейкьявик",
+    link: "https://3pulse.com/uploads/photo/19/99/00/2020/01/22/0230dba935.jpg",
+  },
+  {
+    name: "Нью-Йорк",
+    link: "https://upload.wikimedia.org/wikipedia/commons/8/83/The_Vessel_%28top-down_view%29%2C_Hudson_Yards%2C_New_York_City%2C_July_2019.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Каир",
+    link: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Cairo_Tower_2011.JPG/135px-Cairo_Tower_2011.JPG",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Польша",
+    link: "https://a.d-cd.net/_EAAAgGPvuA-1920.jpg",
+  },
+];
+
 export const buttonEditProfile = document.querySelector(".profile__edit-button");
 export const buttonAddCard = document.querySelector(".profile__add-button");
 export const formProfile = document.querySelector(".popup__form_type_profile");
