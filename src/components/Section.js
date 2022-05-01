2e7d606bbce69d24894b11aca5802bcd3cdca703
Olga Strelkova <86570826+OlgaStrelk@users.noreply.github.com>
2022-05-01 22:12:26 +0300

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 //const cardsContainer = document.querySelector(".cards");
 //items — это массив данных, которые нужно добавить на страницу при инициализации класса.
-//Свойство renderer 
+//Свойство
 export class Section {
   constructor( renderer, container) {
     this._container = container;
