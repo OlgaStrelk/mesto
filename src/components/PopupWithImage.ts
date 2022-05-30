2f7b4c879b8e4d80a8f2ccea1fc06d364e8a4d52
Strelkova Olga <strelod@yandex.ru>
2022-05-30 05:50:13 +0300

    feat ts for popupwithform

--- src/components/PopupWithImage.ts
+++ src/components/PopupWithImage.ts
@@ -1,7 +1,7 @@
 import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
-  open(url, text) {
-    const image = this._popup.querySelector(".popup__image");
+  open(url: string, text: string): void {
+    const image: HTMLInputElement = this._popup.querySelector(".popup__image");
     const caption = this._popup.querySelector(".popup__caption");
 
     image.src = url
